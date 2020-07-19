478cd280c15b2f3cade3c3a352e26667ec5af270
domanisamuel <wafulasammie@gmail.com>
2020-07-19 20:59:09 +0300

    [Feature]: Login with (jwt)

--- controllers/users.js
+++ controllers/users.js
@@ -4,6 +4,8 @@
 const User = require('../models/users');
 // encrypt
 const bcrypt = require('bcryptjs');
+// json web tokens
+const jwt = require('jsonwebtoken');
 
 // save a user (signup)
 exports.create = (req, res) => {
@@ -82,6 +84,29 @@ exports.delete = (req, res) => {
     User.deleteOne({ _id:req.params.id })
     .then((user) => {
         res.status(200).json({ message:`user deleted!` })
+    })
+    .catch((error) => {
+        res.status(404).json(error)
+    })
+}
+
+// (Auth) login
+exports.login = (req, res) => {
+    User.findOne({ email: req.body.email })
+    .then((user) => { // check whether email exists
+        if(!user) {
+            res.status(404).json({ message:`user not found` })
+        } else {
+            // compare passwords
+            const passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
+            if(!passwordIsValid) { 
+                return res.status(401).json({ auth: false, message:`wrong password or email` });
+            } else {
+                // generate token
+                const token = jwt.sign( { userId: user._id }, 'RANDOM_TOKEN_SECRET', { expiresIn: 900 }); // expires in 15 min
+                res.status(200).json({ auth:true, token: token })
+            }
+        }
     })
     .catch((error) => {
         res.status(404).json(error)
