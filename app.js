7f6f3bda649914d53c06a0dba1cc20f7d5d5ceb5
domanisamuel <wafulasammie@gmail.com>
2020-07-23 19:04:15 +0300

    auth

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
+const config = require('./config')
 
 // bodyparser
 var bodyParser = require('body-parser');
@@ -9,7 +10,7 @@ app.use(bodyParser.urlencoded ({ extended: true }));
 
 // connect to db
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://admin:y6bbhreN@ds263108.mlab.com:63108/kiki', { useNewUrlParser: true, useUnifiedTopology: true  })
+mongoose.connect(`${config.DATABASE}`, { useNewUrlParser: true, useUnifiedTopology: true  })
 .then(() => {
     console.log('successfully connected')
 })
---
+++ config.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+    'SECRET': 'supersecret',
+    'DATABASE': '' //You can set whatever your db
+};
